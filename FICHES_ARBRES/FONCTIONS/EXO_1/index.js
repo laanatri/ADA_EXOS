fe6110972a9386977ab2846fc619bba04f98e2d3
laanatri <laanatri.ade@gmail.com>
2025-01-29 13:12:36 +0100

    Fonctions exo 1

---
+++ FICHES_ARBRES/FONCTIONS/EXO_1/index.js
@@ -0,0 +1,19 @@
+const addition3Numbers = () => {
+    console.log("Le résultat de la somme est", 1 + 2 + 3);
+};
+
+addition3Numbers();
+
+const n1 = 54;
+const n2 = 10;
+const n3 = 1;
+
+const addition3NumbersWithArg = (num1, num2, num3) => {
+    console.log("Le résultat de la somme est", num1 + num2 + num3);
+};
+
+addition3NumbersWithArg(n1, n2, n3);
+
+addition3NumbersWithArg(49, 24, 10);
+addition3NumbersWithArg(76, 4, 4);
+addition3NumbersWithArg(87, 17, 5);
