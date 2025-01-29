1ae68710430976e75cecbf6ef233d4103f421f79
laanatri <laanatri.ade@gmail.com>
2025-01-29 11:02:07 +0100

    Variable exo 3

---
+++ FICHES_ARBRES/SYNTAXE_VARIABLE_ORDREDEXECUTION/EXO_3/index.js
@@ -0,0 +1,7 @@
+//firstLanguage = "Javascript"
+//secondLanguage = "Javascript"
+//thirdLanguage = "HTML"
+
+//firstLanguage = "Javascript"
+//secondLanguage = "Javascript"
+//thirdLanguage = "Javascript"
