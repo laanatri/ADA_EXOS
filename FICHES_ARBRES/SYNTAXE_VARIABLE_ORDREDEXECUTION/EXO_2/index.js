ba875ad7654c95a3b83d949d61a3febc03e9eb5e
laanatri <laanatri.ade@gmail.com>
2025-01-29 10:54:35 +0100

    variables exo 2

---
+++ FICHES_ARBRES/SYNTAXE_VARIABLE_ORDREDEXECUTION/EXO_2/index.js
@@ -0,0 +1,10 @@
+const myName = "adelaide";
+const mySurname = "laanatri";
+
+alert(`Bonjour, ${myName} ${mySurname}`);
+
+const numValue = 64;
+const myAge = 37;
+const valueMinusMyAge = numValue - myAge;
+
+alert(`${valueMinusMyAge} années à travailler avant la retraite ! Courage ! En développant ce sera plus fun !`);
