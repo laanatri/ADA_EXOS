79f459b4f0cd71d6f7b551b75d2e8e73c44184eb
laanatri <laanatri.ade@gmail.com>
2025-02-05 11:55:38 +0100

    protocole web exo 1

---
+++ FICHES_ARBRES/PROTOCOLES_WEB/EXO_1/index.js
@@ -0,0 +1,35 @@
+// $ curl --head https://www.adatechschool.fr
+// ligne de commande
+
+// HTTP/1.1 301 Moved Permanently
+// la version du protocole HTTP et le statut HTTP, l'url a changé
+
+// Date: Mon, 03 Feb 2025 09:04:14 GMT
+// le moment ou le serveur répond
+
+// Content-Type: text/html; charset=UTF-8
+// le contenu est un page HTML en encodage UTF-8
+
+// Connection: keep-alive
+// garde la connexion ouverte pour d'autre requêtes
+
+// Server: OVHcloud
+// l'hébergement est sur un serveur OVH
+
+// X-Powered-By: PHP/8.2
+// le serveur utilise PHP 8.2
+
+// Expires: Mon, 03 Feb 2025 10:04:14 GMT
+// expiration du cache
+
+// Cache-Control: max-age=3600
+// la réponse pet être mise en cache pendant 3600s
+
+// X-Redirect-By: WordPress
+// rediriger par Wordpress
+
+// Location: https://adatechschool.fr/
+// nouvelle url où est le site
+
+// Vary: Accept-Encoding
+// la réponse peut varier en fonction de l'encodage
