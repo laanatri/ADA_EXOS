f8eb1fd6a5bc2627b24a964839288037e7db8cb8
laanatri <laanatri.ade@gmail.com>
2025-02-13 11:39:31 +0100

    boucles exo 1

---
+++ FICHES_ARBRES/BOUCLES/EXO_1/script.js
@@ -0,0 +1,28 @@
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+// }
+
+
+
+// let i = 0;
+
+// while (i < 100) {
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// Je pense que le for est plus efficace et on voit les conditions en une ligne
+
+
+
+// for (let i = 50; i < 100; i++) {
+//     console.log(i);
+// }
+
+
+
+for (let i = 49; i > 0; i--) {
+    console.log(i);
+}
