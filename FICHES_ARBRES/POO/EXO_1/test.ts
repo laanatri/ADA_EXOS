f87398ef4d91b44ddfced322d7afbc0801dff76f
laanatri <laanatri.ade@gmail.com>
2025-06-25 20:31:01 +0200

    update poo exo 1

--- FICHES_ARBRES/POO/EXO_1/test.ts
+++ FICHES_ARBRES/POO/EXO_1/test.ts
@@ -1,10 +1,10 @@
 class Director {
-    firstname: string;
-    lastname: string;
-    birthYear: number;
-    age: number;
+    public firstname: string;
+    public lastname: string;
+    public birthYear: number;
+    public age: number;
 
-    constructor(firstname: string, lastname: string, birthYear: number, age: number) {
+    public constructor(firstname: string, lastname: string, birthYear: number, age: number) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.birthYear = birthYear;
@@ -19,46 +19,56 @@ enum TypeCategorie {
 }
 
 class Categorie {
-    name: string;
-    type: TypeCategorie;
+    private name: string;
+    private type: TypeCategorie;
 
-    constructor(name: string, type: TypeCategorie) {
+    public constructor(name: string, type: TypeCategorie) {
         this.name = name;
         this.type = type;
     }
 }
 
 class Film {
-    title: string;
-    releaseYear: number;
-    director: Director;
-    categories: Categorie[];
+    private title: string;
+    private releaseYear: number;
+    private director: Director;
+    private categories: Categorie[];
 
-    constructor(title: string, releaseYear: number, director: Director, categories: Categorie[]) {
+    public constructor(title: string, releaseYear: number, director: Director, categories: Categorie[]) {
         this.title = title;
         this.releaseYear = releaseYear;
         this.director = director;
         this.categories = categories
     }
 
-    pitch() {
+    public pitch() {
         console.log(`Le film ${this.title} est sorti en ${this.releaseYear} et est réalisé par ${this.director.firstname} ${this.director.lastname}.`)
     }
 }
 
-class Customer {
-    firstname: string;
-    lastname: string;
-    movies: Film[];
+class Rental {
+    private movie: Film;
+    private date: Date;
 
-    constructor(firstname: string, lastname: string, movies: Film[]) {
+    public constructor(movie : Film) {
+        this.movie = movie;
+        this.date = new Date();
+    }
+}
+
+class Customer {
+    private firstname: string;
+    private lastname: string;
+    public rentedMovies: Rental[];
+
+    public constructor(firstname: string, lastname: string) {
         this.firstname = firstname;
         this.lastname = lastname;
-        this.movies = movies
+        this.rentedMovies = [];
     }
 
-    addRent(film: Film) {
-        this.movies.push(film);
+    public addRent(movie: Film): void {
+        this.rentedMovies.push(new Rental(movie));
     }
 }
 
@@ -76,8 +86,8 @@ film1.pitch();
 film2.pitch();
 film3.pitch();
 
-let customer1 = new Customer("Adé", "Laana", [film2]);
-console.log(customer1.movies);
+let customer1 = new Customer("Adé", "Laana");
+console.log(customer1.rentedMovies);
 
 customer1.addRent(film3);
-console.log(customer1.movies);
+console.log(customer1.rentedMovies);
